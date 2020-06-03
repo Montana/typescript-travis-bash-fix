92b9b4cde42c666dc4fc5f163cb4b33a9e026964
Montana Mendy <montana@getprowl.com>
2020-06-03 01:27:07 -0700

    bash script for travis deployed via terminal

---
+++ tsx.sh
@@ -0,0 +1,31 @@
+if [[ $# < 2 ]]; then
+    p="$(basename $0)"
+    here=$(mktemp)
+    git remote -v 2>/dev/null | grep -oP '(?<=github.com[:/])([^/]+/[^/]+?)(?=\.git| )' > "$here"
+    IFS=/ read user repo < "$here"
+else
+    user="$1"
+    repo="$2"
+    shift 2
+fi
+
+if [[ -z "$user" || -z "$repo" ]]; then
+    echo "usage:  $p [user] [repository] [value to encrypt]"
+    echo
+    echo "  e.g.: $p 'P@ssw0rd' (only inside a repo with a github remote)"
+    echo "    or  $p ${user:-jsmith} ${repo:-MyRepo} 'VAR=\"s3cret\"'"
+    echo "    or  $p ${user:-jsmith} ${repo:-MyRepo} 'P@ssw0rd'"
+    exit 1
+fi >&2
+
+value="$1"
+
+# Fetch key
+keyurl="https://api.travis-ci.org/repos/$user/$repo/key"
+echo "Fetching key from $keyurl ..." >&2
+keyfile=$(mktemp)
+keyfile2=$(mktemp)
+curl -s "$keyurl" > "$keyfile" || {
+    echo "Couldn't fetch key from $keyurl!"
+    exit 1
+} >&2
